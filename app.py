81fc084c383ba88b318ef78715060d27f3f93273
FatherOfEgg <fatherofegg@gmail.com>
2024-10-01 22:16:46 -0400

    Basic Dash & LlamaIndex interaction

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from llama_index.core import (
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
 
+from dash import (
+    Dash,
+    html,
+    Input,
+    Output,
+    callback,
+    dcc
+)
+
 llm = Groq(model="llama3-70b-8192", api_key="YOUR_API_KEY")
 
 Settings.llm = llm
@@ -27,6 +36,30 @@ else:
     index = load_index_from_storage(storage_context)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What are the top 5 popular breeds?")
+# response = query_engine.query("What are the top 5 popular breeds?")
 
-print(response)
+# print(response)
+
+app = Dash()
+
+app.layout = html.Div(
+    [
+        dcc.Input(id="input1", type="text", placeholder="Enter prompt", debounce=True),
+        html.Div(id="output"),
+    ]
+)
+
+@callback(
+    Output("output", "children"),
+    Output("input1", "value"), # https://stackoverflow.com/q/72351437
+    Input("input1", "value"),
+)
+def update_output(input1):
+    response = ""
+    if input1:
+        response = query_engine.query(input1)
+
+    return f"Response: {response}", ""
+
+if __name__ == '__main__':
+    app.run(debug=True)
