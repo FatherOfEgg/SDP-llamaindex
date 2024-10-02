0fd8b4be0d6d085f1ae59c7ef1375c4fbaef3422
FatherOfEgg <fatherofegg@gmail.com>
2024-10-01 21:13:11 -0400

    Added persistent storage

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os.path
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
-    Settings
+    StorageContext,
+    Settings,
+    load_index_from_storage
 )
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
@@ -13,8 +16,16 @@ Settings.embed_model = HuggingFaceEmbedding(
     model_name="BAAI/bge-small-en-v1.5"
 )
 
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
+PERSIST_DIR = "./storage"
+
+if not os.path.exists(PERSIST_DIR):
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
 query_engine = index.as_query_engine()
 response = query_engine.query("What are the top 5 popular breeds?")
 
