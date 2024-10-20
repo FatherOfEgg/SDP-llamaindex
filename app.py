be65871756531ddde75263c8125e8593a6128131
FatherOfEgg <fatherofegg@gmail.com>
2024-10-20 18:30:26 -0400

    Basic chat history and added some bonebase data

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from llama_index.core import (
 from llama_parse import LlamaParse
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core.storage.chat_store import SimpleChatStore
+from llama_index.core.chat_engine.types import ChatMode
 
 from dash import (
     Dash,
@@ -27,22 +30,44 @@ llm = Groq(model="llama3-70b-8192", api_key=os.getenv("GROQ_API_KEY"))
 
 Settings.llm = llm
 Settings.embed_model = HuggingFaceEmbedding(
-    model_name="BAAI/bge-small-en-v1.5"
+    # model_name="BAAI/bge-small-en-v1.5"
+    # model_name="google/tapas-large-finetuned-wtq"
+    # model_name="deepset/bert-base-cased-squad2"
+    # model_name="google-bert/bert-large-uncased-whole-word-masking-finetuned-squad"
+    model_name="BAAI/bge-base-en-v1.5"
 )
 
 PERSIST_DIR = "./storage"
 
 if not os.path.exists(PERSIST_DIR):
-    documents = LlamaParse().load_data("./data/Registered-Cats-By-Breed.2022.pdf")
+    documents = LlamaParse().load_data([
+        "./data/Registered-Cats-By-Breed.2022.pdf",
+        "./data/bonebase bvtv.docx"
+    ])
     index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
     index = load_index_from_storage(storage_context)
 
-query_engine = index.as_query_engine()
+# query_engine = index.as_query_engine()
 
-# Based on https://github.com/AdamSpannbauer/app_rasa_chat_bot
+chat_store = SimpleChatStore.from_persist_path(
+    persist_path="chat_store.json"
+)
+
+memory = ChatMemoryBuffer.from_defaults(
+    token_limit=3000,
+    chat_store=chat_store,
+    chat_store_key="user1",
+)
+
+chat_engine = index.as_chat_engine(
+    chat_mode=ChatMode.CONTEXT,
+    memory=memory,
+)
+
+# chat_engine = index.as_chat_engine(memory=chat_memory)
 
 app = Dash()
 
@@ -51,22 +76,24 @@ conv_hist = []
 app.layout = html.Div([
     html.H3("BoneGPT", style={"text-align": "center"}),
     html.Div([
-        html.Br(),
-        html.Div(id="conversation")],
-        id="chat",
-        style={"width": "400px", "margin": "0 auto"}
-    ),
-    html.Div([
-        html.Table([
-            html.Tr([
-                html.Td([dcc.Input(id="prompt", placeholder="Enter a prompt", type="text")],
-                        style={"valign": "middle"}),
-                html.Td([html.Button("Send", id="send_button", type="submit")],
-                        style={"valign": "middle"})
-                ])
-            ])],
-            id="user-input",
-            style={"width": "325px", "margin": "0 auto"}),
+        html.Div([
+            html.Br(),
+            html.Div(id="conversation")],
+                 id="chat",
+                 style={"width": "400px", "margin": "0 auto"}
+                 ),
+        html.Div([
+            html.Table([
+                html.Tr([
+                    html.Td([dcc.Input(id="prompt", placeholder="Enter a prompt", type="text")],
+                            style={"valign": "middle"}),
+                    html.Td([html.Button("Send", id="send_button", type="submit")],
+                            style={"valign": "middle"})
+                    ])
+                ])],
+                id="user-input",
+                style={"width": "325px", "margin": "0 auto"}),
+    ])
 ])
 
 @callback(
@@ -79,9 +106,12 @@ def update_conversation(click, text):
 
     if click and click > 0:
         prompt = [html.H5(text, style={"text-align": "right"})]
-        response = [html.H5(str(query_engine.query(text)), style={"text-align": "left"})]
+        # response = [html.H5(str(query_engine.query(text)), style={"text-align": "left"})]
+        response = [html.H5(str(chat_engine.chat(text)), style={"text-align": "left"})]
 
         conv_hist += prompt + response
+
+        # chat_store.persist(persist_path="chat_store.json")
 
         return conv_hist
     else:
